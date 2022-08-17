d144e9fd44a5f8f36044825fd00481cd08e252db
Martin Kaněra <marti.kanera@gmail.com>
2022-08-17 13:10:39 +0200

    feat: use graph context in toolbar

--- src/Toolbar.js
+++ src/Toolbar.js
@@ -1,11 +1,31 @@
+import { useState, useContext } from 'react';
+import GraphContext from './GraphContext';
 import './Toolbar.css';
 
 function Toolbar ({ zoomOut, zoomIn, reset }) {
+  const [copied, setCopied] = useState(false);
+  const graph = useContext(GraphContext);
+
+  const copyJSONToClipboard = async () => {
+    await navigator.clipboard.writeText(JSON.stringify(graph.current.toJSON()));
+    setCopied(true);
+    setTimeout(() => setCopied(false), 3000);
+  };
+
   return (
   <div className="toolbar" style={{ position: 'absolute', zIndex: 100 }}>
     <button onClick={zoomOut}>Zoom Out</button>
     <button onClick={zoomIn}>Zoom In</button>
     <button onClick={reset}>Reset</button>
+    <button
+      onClick={copyJSONToClipboard}
+      style={{ 
+        backgroundColor: copied ? '#019e01' : '#F8F9FB',
+        color: copied ? '#fff' : '#000',
+      }}
+    >
+      {copied ? 'Copied' : 'Copy'}
+    </button>
   </div>
   )
 }
